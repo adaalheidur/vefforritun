928c87fea6c98c56443e60c2fda802a005210ffd
adaalheidur <111927564+adaalheidur@users.noreply.github.com>
2022-11-16 13:31:42 +0000

    Add files via upload

---
+++ v10/vite-project/main.js
@@ -0,0 +1,27 @@
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+context.fillStyle = 'yellow';
+context.beginPath();
+context.arc(320, 240, 200, 0, 2 * Math.PI);
+context.fill();
+context.closePath();
+
+// augu
+context.fillStyle = 'black';
+context.beginPath();
+context.arc(270, 175, 30, 0, 2 * Math.PI);
+context.fill();
+context.closePath();
+
+context.beginPath();
+context.arc(370, 175, 30, 0, 2 * Math.PI);
+context.fill();
+context.closePath();
+
+// munnur
+context.strokeStyle = 'black';
+context.lineWidth = 5;
+context.beginPath();
+context.arc(320, 240, 150, 0, -1 * Math.PI);
+context.stroke();
+context.closePath();
---
+++ v10/vite-project2/main.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'yellow';
+ctx.fillRect(200, 400, 400, 400);
+
+ctx.fillStyle = 'blue';
+ctx.fillRect(400, 700, 80, 100);
+
+ctx.fillStyle = 'brown';
+ctx.fillRect(800, 600, 20, 100);
+
+ctx.fillStyle = 'green';
+ctx.fillRect(790, 600, 50, 40);
+
+ctx.fillStyle = 'lightblue';
+ctx.fillRect(300, 500, 80, 100);
+
+ctx.fillStyle = 'lightblue';
+ctx.fillRect(500, 500, 80, 100);
